8dbbcb47ae694a7d572bdd2966914480dd9e6363
Rudolf Shenfeld <nerudxlf@gmail.com>
2022-01-24 11:27:16 +0600

    fix bugs

--- DataCalc/dc_Elibrary.py
+++ DataCalc/dc_Elibrary.py
@@ -45,16 +45,27 @@ class DataCalculationElibrary(DataCalculation):
     def count_values(self):
         employees_dict = self.get_employees_dict()
         for authors in self.data_df["Авторы"].to_list():
+            tmp_dict = {}
+            s = 0
+            tmp_authors = []
             for i in authors.split(", "):
                 name = self.__get_name(i)
-                if not name:
-                    break
+                if not name or name in tmp_authors:
+                    continue
+                tmp_authors.append(name)
                 for keys, values in employees_dict.items():
                     if values.get(name):
-                        if self.department_values.get(keys):
-                            self.department_values[keys] += values[name]
+                        if tmp_dict.get(keys):
+                            s += values[name]
+                            tmp_dict[keys] += values[name]
                         else:
-                            self.department_values |= {keys: values[name]}
+                            s += values[name]
+                            tmp_dict |= {keys: values[name]}
+            for keys, values in tmp_dict.items():
+                if self.department_values.get(keys):
+                    self.department_values[keys] += values / s
+                else:
+                    self.department_values |= {keys: values / s}
         return self.department_values
 
 
--- DataCalc/dc_Scopus.py
+++ DataCalc/dc_Scopus.py
@@ -5,14 +5,25 @@ class DataCalculationScopus(DataCalculation):
     def count_values(self):
         employees_dict = self.get_employees_dict()
         for authors in self.data_df["Авторы"].to_list():
+            tmp_dict = {}
+            s = 0
+            tmp_authors = []
             for i in authors.split(", "):
                 name = self.find_in_dictionary(i)
-                if not name:
-                    break
+                if not name or name in tmp_authors:
+                    continue
+                tmp_authors.append(name)
                 for keys, values in employees_dict.items():
                     if values.get(name):
-                        if self.department_values.get(keys):
-                            self.department_values[keys] += values[name]
+                        if tmp_dict.get(keys):
+                            s += values[name]
+                            tmp_dict[keys] += values[name]
                         else:
-                            self.department_values |= {keys: values[name]}
+                            s += values[name]
+                            tmp_dict |= {keys: values[name]}
+            for keys, values in tmp_dict.items():
+                if self.department_values.get(keys):
+                    self.department_values[keys] += values / s
+                else:
+                    self.department_values |= {keys: values / s}
         return self.department_values
